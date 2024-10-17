4f6731b16b65970b6f0306e271a34f613e56bc1b
pappukrs <digitalpappu101@gmail.com>
2024-10-17 19:12:44 +0530

    DAY2

---
+++ DAY2/project/target/debug/deps/project.d
@@ -0,0 +1,5 @@
+F:\GITHUB-REPO\Rust-programming\Rust_Fundamental\DAY2\project\target\debug\deps\project.exe: src/main.rs
+
+F:\GITHUB-REPO\Rust-programming\Rust_Fundamental\DAY2\project\target\debug\deps\project.d: src/main.rs
+
+src/main.rs:
